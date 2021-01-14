ba9426528e30ddccb4445f359c77c720c706394c
AdrianVazquezReyes <adrianvazquez3824@gmail.com>
2021-01-14 16:52:02 -0600

    this is the second commit

--- hi.py
+++ hi.py
@@ -1,2 +1,7 @@
 #This program is to test the comunication between local repo and remote repo
 print('hello from my local computer one')
+a = input('are you hungry?')
+if a == 'yes':
+    print('eat something')
+else:
+    print('do your homework')
